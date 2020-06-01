cf78228d31001eab65f85c60f3eaedf375e390c5
Brian Burkard <brianburkard@gmail.com>
2020-05-31 21:34:44 -0400

    Added dealing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+'use strict';
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var { cards } = require('./Cards');
+
+// initialize in global scope.
+var gameIterator, game;
 
 // app.get('/', function (req, res) {
 //     res.sendFile(__dirname + '/gofish-web/dist/index.html');
@@ -13,6 +17,7 @@ var { cards } = require('./Cards');
 // game.on
 
 
+
 io.on('connection', function (socket) {
     console.log('socket connected', socket.id);
 
@@ -21,12 +26,23 @@ io.on('connection', function (socket) {
     });
 
     // socket.on REQUEST
-    socket.on('REQUEST', (event) => {
+    socket.on('REQUEST', async (event) => {
         console.log('Request for ', event);
-        const { state } = reducer(event);
-        if (state) {
-            socket.emit('RECEIVE', state, event);
+        //console.log(socket.id);
+        try {
+
+            // make changes to game object
+            await reducer(event, socket.id);
+
+            // return game object, and event name originally recieved.
+            // socket.emit('RECEIVE', game, event);
+            io.emit('RECEIVE', game, event);
+
+        } catch (e) {
+            console.error('error: ', e);
         }
+
+        //}
     });
 
 
@@ -40,22 +56,104 @@ http.listen(port, function () {
 
 
 // Card Logic
-function reducer(action) {
+function reducer(action, id) {
     switch (action) {
-        case "DECK":
-            return { state: cards }
         case "SHUFFLE":
-            let shuffled = shuffle(cards);
-            return { state: shuffled };
+            game.shuffle();
+            break;
+        // case "DECK":
+        //     return { state: cards }
+        case "TEST":
+            console.log(game);
+            break;
+        case "START":
+            //gameIterator = newGame();
+            game = new GoFish();
+            break;
+        case "DEAL":
+            game.deal();
+            break;
+        case "JOIN_PLAYER1":
+            game.player1.id = id;
+            break;
+        case "JOIN_PLAYER2":
+            game.player2.id = id;
+            break;
+        //return gameObject(game);
+        // case "NEXT":
+        //     gameIterator.next();
+        //     //return gameObject(game);
+        //     break;
         default:
-            return cards;
+            break;
+        //return cards;
     }
 }
+
+class GoFish {
+    constructor() {
+        this.deck = [...cards];
+        this.player1 = new Player('player1');
+        this.player2 = new Player('player2');
+        this.turn = this.player1.id;
+    }
+
+    deal() {
+        this.shuffle();
+        for (let i = 0; i < 7; i++) {
+            this.draw(this.player1);
+            this.draw(this.player2);
+        }
+        console.log(this.player1);
+        console.log(this.player2);
+    }
+
+    shuffle() {
+        this.deck = this.deck.sort(() => .5 - Math.random());
+    }
+
+    draw(player) {
+        let card = this.deck.pop();
+        card.owner = player.id;
+        player.hand.push(card);
+    }
+}
+
+class Player {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.hand = [];
+        this.books = [];
+        this.turn = false;
+    }
+}
+
+// Game Generator
+// function* newGame() {
+//     game = new GoFish();
+//     game.deal();
+//     //console.log(game.deck.length);
+//     yield console.log(game);
+//     console.log('end');
+//     return;
+// }
+
+
 
 //var { cards } = Cards;
 //console.log(cards);
 
 
-
+//
 // helper functions
-const shuffle = arr => arr.sort(() => .5 - Math.random());
+//
+const gameObject = (game) => {
+    return {
+        deck: game.deck,
+        player1: game.player1,
+        player2: game.player2
+    }
+}
+
+//const shuffle = arr => arr.sort(() => .5 - Math.random());
